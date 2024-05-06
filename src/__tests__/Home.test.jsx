aab174c1a06e0836d7573350dab676a6d850cdd0
Sanjaykhanna <sanjaykhanna9122001@gmail.com>
2024-05-06 17:18:02 +0530

    new update commit

--- src/__tests__/Home.test.jsx
+++ src/__tests__/Home.test.jsx
@@ -1,14 +1,14 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';;
 import Home from '../Components/Home';
 
 
-    test('should render the home page', () => {
+    it('should render the home page', () => {
         render(<Home />)
         expect(screen.getByTitle("title")).toBeInTheDocument();
         expect(screen.getByTitle("title")).toHaveTextContent("Hotel Management System");
     })
-    test('should render header page', () => {
+    it('should render header page', () => {
         render(<Home />)
         expect(screen.getByTitle("pageTitle")).toBeInTheDocument();
         expect(screen.getByTitle("pageTitle")).toHaveTextContent("Hotel List");
